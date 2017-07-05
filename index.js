9757ec69660f5fbfc8735c9445c660df9d441ed9
Aleksey Dorokhov <akeinhell@gmail.com>
2017-07-05 16:55:36 +0700

    add express

--- index.js
+++ index.js
@@ -112,3 +112,15 @@ bot.on('message', (msg) => {
           }
       });
 });
+
+var express = require('express');
+var app = express();
+
+app.get('/', function (req, res) {
+    res.send('Hello');
+});
+
+let port = process.env.PORT || 80;
+app.listen(port, () => {
+    console.log(`listen port ${port}`);
+});
