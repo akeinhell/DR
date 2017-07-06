dd7e1e149481378c6b6a0629e587074407ec369c
Aleksey Dorokhov <akeinhell@gmail.com>
2017-07-06 12:55:49 +0700

    eslint

--- index.js
+++ index.js
@@ -5,16 +5,14 @@ import _debug from 'debug';
 const debug = new _debug('quest');
 const bodyParser = require('body-parser');
 
-let TOKEN = process.env.TELEGRAM_TOKEN;
-let rtg   = require("url").parse(process.env.REDIS_URL);
+const TOKEN = process.env.TELEGRAM_TOKEN;
+const rtg   = require('url').parse(process.env.REDIS_URL);
 
-console.log(rtg);
-
-let cache = redis.createClient({
-    port: rtg.port,
-    host: rtg.hostname
+const cache = redis.createClient({
+  port: rtg.port,
+  host: rtg.hostname
 });
-cache.auth(rtg.auth.split(":")[1]);
+cache.auth(rtg.auth.split(':')[1]);
 
 bluebird.promisifyAll(redis.RedisClient.prototype);
 bluebird.promisifyAll(redis.Multi.prototype);
@@ -22,13 +20,13 @@ bluebird.promisifyAll(redis.Multi.prototype);
 const bot = new TelegramBot(process.env.TELEGRAM_TOKEN);
 
 const answer = {
-    correct: 'Отлично, го дальше :-)',
-    wrong: 'Хм... подумай еще',
-    finish: 'Ура...!!!! C днем рождения'
+  correct: 'Отлично, го дальше :-)',
+  wrong: 'Хм... подумай еще',
+  finish: 'Ура...!!!! C днем рождения'
 };
 
 const quests = [
-    {id: 0, text: `Привет, у меня есть к тебе срочное дело. В этом можешь помочь только ты.
+  {id: 0, text: `Привет, у меня есть к тебе срочное дело. В этом можешь помочь только ты.
 Для начала давай познакомимся.
     Я даритель подарков. И я приготовил для тебя подарок.
     Но для того чтобы ты его получила, тебе нужно ответить на несколько вопросов
@@ -38,97 +36,86 @@ const quests = [
     *Сколько будет три минус два*
     ФО: Слово
     `,
-        answer: 'один'},
-    {id: 1, text: 'а 1+1?', answer: 'два'},
-    {id: 2, text: 'Какая молодец... А сколько будет 5-3?', answer: 'три'},
-    {id: 3, text: 'дважды два', answer: 'четыре'},
-    {id: 4, text: 'сколько пальцев на руке', answer: 'пять'},
-    {id: 5, text: 'Прошлый ответ плюс один', answer: 'шесть'},
+    answer: 'один'},
+  {id: 1, text: 'а 1+1?', answer: 'два'},
+  {id: 2, text: 'Какая молодец... А сколько будет 5-3?', answer: 'три'},
+  {id: 3, text: 'дважды два', answer: 'четыре'},
+  {id: 4, text: 'сколько пальцев на руке', answer: 'пять'},
+  {id: 5, text: 'Прошлый ответ плюс один', answer: 'шесть'},
 ];
 
 bot.onText(/\/start/, (msg) => {
-    let chatId = msg.chat.id;
-    setState(chatId, 0)
-      .then(async () => {
-          let quest = await getQuest(chatId);
-          sendQuest(chatId, quest);
-      })
-      .catch(() => {});
+  const chatId = msg.chat.id;
+  setState(chatId, 0)
+    .then(async () => {
+      const quest = await getQuest(chatId);
+      sendQuest(chatId, quest);
+    })
+    .catch(() => {});
 });
 
-const debugMessage = (chatId, msg) => {
-    debug(`debugMessage chatid: ${chatId}`);
-    debug(`debugMessage msg: ${msg}`);
-    let chatKey = `DR:${chatId}:DEBUG`;
-    cache.getAsync(chatKey)
-      .then(data => data && bot.sendMessage(chatId, `*DEBUG*\n${msg}`, {parse_mode: 'Markdown'}))
-};
-
 const setState = async (chatId, state) => {
-    debug(`set state ${chatId} = ${state}`);
-    let chatKey = `DR:${chatId}:STATE`;
-    return await cache.setAsync(chatKey, state);
+  debug(`set state ${chatId} = ${state}`);
+  const chatKey = `DR:${chatId}:STATE`;
+  return await cache.setAsync(chatKey, state);
 };
 
 const getState = async (chatId) => {
-    let chatKey = `DR:${chatId}:STATE`;
-    let state = parseInt(await cache.getAsync(chatKey));
-    debug(`getState ${chatId} = ${state}`);
-    return state;
+  const chatKey = `DR:${chatId}:STATE`;
+  const state = parseInt(await cache.getAsync(chatKey));
+  debug(`getState ${chatId} = ${state}`);
+  return state;
 };
 
 const getQuest = async (chatId) => {
-    let state = await getState(chatId);
-    debug(`getQuest: state = ${state}`);
-    return quests.find(i => i.id == state);
+  const state = await getState(chatId);
+  debug(`getQuest: state = ${state}`);
+  return quests.find(i => i.id == state);
 };
 
 const sendQuest = (chatId, quest, add = false) => {
-    debug(`sendQuest ${JSON.stringify(quest)}`);
-    let msg = add? `*${answer.correct}*\n\n${quest.text}` : quest.text;
-    msg && bot.sendMessage(chatId, msg, {parse_mode: 'Markdown'});
-}
+  debug(`sendQuest ${JSON.stringify(quest)}`);
+  const msg = add? `*${answer.correct}*\n\n${quest.text}` : quest.text;
+  msg && bot.sendMessage(chatId, msg, {parse_mode: 'Markdown'});
+};
 
 bot.on('message', (msg) => {
-    const chatId = msg.chat.id;
-    getQuest(chatId)
-      .then(async (quest) => {
-          if (msg.text == '/start') {
-              return;
-          }
-          if (!quest) {
-              await setState(chatId, 0); // @todo delete
-              return bot.sendMessage(chatId, `*Hura!!!!*`, {parse_mode: 'Markdown'});
-          }
+  const chatId = msg.chat.id;
+  getQuest(chatId)
+    .then(async (quest) => {
+      if (msg.text == '/start') {
+        return;
+      }
+      if (!quest) {
+        await setState(chatId, 0); // @todo delete
+        return bot.sendMessage(chatId, '*Hura!!!!*', {parse_mode: 'Markdown'});
+      }
 
-          if (msg.text.toLowerCase() === quest.answer.toLowerCase()) {
-              let state = await getState(chatId);
-              console.log('state', state);
-              await setState(chatId, state + 1);
-              let newQuest = await getQuest(chatId);
-              console.log('newQ', newQuest);
-              if (newQuest) {
-                  sendQuest(chatId, newQuest, answer.correct)
-              } else {
-                  bot.sendMessage(chatId, `${answer.finish}`, {parse_mode: 'Markdown'});
-              }
-          } else {
-              bot.sendMessage(chatId, `*${answer.wrong}*`, {parse_mode: 'Markdown'});
-          }
-      });
+      if (msg.text.toLowerCase() === quest.answer.toLowerCase()) {
+        const state = await getState(chatId);
+        await setState(chatId, state + 1);
+        const newQuest = await getQuest(chatId);
+        if (newQuest) {
+          sendQuest(chatId, newQuest, answer.correct);
+        } else {
+          bot.sendMessage(chatId, `${answer.finish}`, {parse_mode: 'Markdown'});
+        }
+      } else {
+        bot.sendMessage(chatId, `*${answer.wrong}*`, {parse_mode: 'Markdown'});
+      }
+    });
 });
 
-var express = require('express');
-var app = express();
+const express = require('express');
+const app = express();
 
 app.get('/', function (req, res) {
-    res.sendStatus(201);
+  res.sendStatus(201);
 });
 
-let port = process.env.PORT || 80;
+const port = process.env.PORT || 80;
 
-let url = 'https://nata-dr.herokuapp.com'
-console.log('bot url', `${url}/bot${TOKEN}`);
+const url = 'https://nata-dr.herokuapp.com';
 bot.setWebHook(`${url}/bot${TOKEN}`);
 
 app.use(bodyParser.json());
@@ -140,5 +127,5 @@ app.post(`/bot${TOKEN}`, (req, res) => {
 });
 
 app.listen(port, () => {
-    console.log(`listen port ${port}`);
+  debug(`listen port ${port}`);
 });
